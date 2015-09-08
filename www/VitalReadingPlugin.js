62ef0535cd55ce19e217ec05ea0e0219d5855d45
Neha Neema <neha.neema@advanz101.com>
2015-09-08 16:15:52 +0530

    Updated

--- www/VitalReadingPlugin.js
+++ www/VitalReadingPlugin.js
@@ -1,15 +1,32 @@
-var vitalReading =  {
-    createEvent: function(title,notes, successCallback, errorCallback) {
-        cordova.exec(
-            successCallback, // success callback function
-            errorCallback, // error callback function
-            'VitalReadingPlugin', // mapped to our native Java class called "Calendar"
-            'viewSPReading', // with this action name
-            [{                  // and this array of custom arguments to create our entry
-                "title": title,
-                "description": notes
-            }]
-        );
-    }
-}
-module.exports = vitalReading;
+  cordova.define("cordova-plugin-vitalreading.VitalReadingPlugin", function(require, exports, module)
+  {
+	  var exec = require("cordova/exec");
+      var vitalReadingCall = {
+		  initiateDevice: function(text, successCallback, errorCallback)  { 
+			
+			 exec(
+	            successCallback, // success callback function
+	            errorCallback, // error callback function
+	            "vitalreading", // mapped to our native Java class called "Calendar"
+	            "initialize",
+	            [{                  // and this array of custom arguments to create our entry
+	                "text": text
+	            }] );
+			  },
+      
+       /* Function to Scan Device all object */
+		  startReading: function(type,mac, successCallback, errorCallback)  { 
+ 	      exec(
+          successCallback, // success callback function
+          errorCallback, // error callback function
+          "vitalreading", // mapped to our native Java class called "Calendar"
+          "viewReading",
+          [{                  // and this array of custom arguments to create our entry
+              "mac": mac,
+              "type": type
+              
+          }] );
+ 	    }
+     };
+     module.exports = vitalReadingCall;
+  }); 
